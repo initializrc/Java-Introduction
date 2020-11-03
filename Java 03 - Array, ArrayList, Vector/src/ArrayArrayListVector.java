0d48d7f6ced64e5a943f55e559da43d7375aaa09
initializrc <panji.cahyountoro@gmail.com>
2020-11-03 07:44:18 +0700

    array, arraylist, vector

--- Java 03 - Array, ArrayList, Vector/src/ArrayArrayListVector.java
+++ Java 03 - Array, ArrayList, Vector/src/ArrayArrayListVector.java
@@ -50,5 +50,40 @@ public class ArrayArrayListVector {
 
         // mengubah array menjadi string
         System.out.println(Arrays.toString(arrayString));
+
+        System.out.println("\nDuplicate Array");
+        int[] arraySource = {1,3,5,7};
+
+        int[] arrayTarget1 = new int[4];
+        int[] arrayTarget2 = new int[4];
+        int[] arrayTarget3 = new int[4];
+
+        // 1. dengan looping
+        System.out.println("Duplicate with Looping");
+        for (int i = 0; i < arraySource.length; i++){
+            arrayTarget1[i] = arraySource[i];
+        }
+        // print array
+        System.out.println("Array Target 1 : ");
+        for (int i = 0; i < arrayTarget1.length; i++){
+            System.out.println(arrayTarget1[i]);
+        }
+
+        // 2. dengan arraycopy
+        System.out.println("Duplicate with Array Copy");
+        System.arraycopy(arraySource, 0, arrayTarget2, 0, 4);
+        // print
+        System.out.println("Array Target 2 : ");
+        for (int i = 0; i < arrayTarget2.length; i++){
+            System.out.println(arrayTarget2[i]);
+        }
+
+        // 3. dengan clone
+        System.out.println("Duplicate with Clone");
+        arrayTarget3 = arraySource.clone();
+        System.out.println("Array Target 3 : ");
+        for (int i = 0; i < arrayTarget3.length; i++){
+            System.out.println(arrayTarget3[i]);
+        }
     }
 }
